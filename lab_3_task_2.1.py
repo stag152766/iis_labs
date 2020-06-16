2d946e6ecb523babfa325c3a19b46b9f90324892
stag152766 <45874547+stag152766@users.noreply.github.com>
2020-06-16 23:51:15 +0700

    add task 2.1

--- lab_3_task_2.1.py
+++ lab_3_task_2.1.py
@@ -1,23 +1,24 @@
 # 2.1 Написать программу на языке Python для ручного выделения
 # (прямоугольной рамкой разных цветов) на изображении различных объектов: машин, людей, фонарей…
+import cv2
 
-import cv2 as cv22
+st = []
+image = cv2.imread("C:\\python_\\cp.jpg")
+cv2.imshow('image', image)
+
 
 def click_event(event, x, y, flag, param):
-    if event == cv22.EVENT_LBUTTONDOWN:
-        print(x, ', ', y)
-        font = cv22.FONT_HERSHEY_SIMPLEX
-        strXY = str(x) + ', ' + str(y)
-        cv22.rectangle('image', (x, y), (15, 15), (222, 255, 0), -1)
-        #cv22.putText(img, strXY, (x, y), font, 1, (222, 255, 0), 2)
-        cv22.imshow('image', img)
+    global st
+    if event == cv2.EVENT_LBUTTONDOWN:
+        st = [(x, y)]
+    if event == cv2.EVENT_LBUTTONUP:
+        st.append((x, y))
+        cv2.rectangle(image, st[0], st[1], (222, 255, 0), 2)
+        cv2.imshow('image', image)
 
 
-img = cv22.imread("C:\\python_\\cp.jpg")
-cv22.imshow('image', img)
+cv2.setMouseCallback('image', click_event)
 
-cv22.setMouseCallback('image', click_event)
+cv2.waitKey(0)
 
-cv22.waitKey(0)
-
-cv22.destroyAllWindows()
+cv2.destroyAllWindows()
